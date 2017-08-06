1580d52f756687c48465a413e1f799d77003211e
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-08-06 14:36:31 -0300

    Added MySQL

---
+++ scripts/usdocker_mysql.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const Config = require('../include/config');
+const DockerRunWrapper = require('../include/dockerrunwrapper');
+const usdockerhelper = require('../include/usdockerhelper');
+const showdocs = require('../include/showdocs');
+
+const SCRIPTNAME = 'mysql';
+
+let config = new Config(SCRIPTNAME, '/tmp');
+let configGlobal = new Config(null, '/tmp');
+
+function getContainerDef() {
+
+    let docker = new DockerRunWrapper(configGlobal);
+    return docker
+        .containerName(SCRIPTNAME + configGlobal.get('container-suffix'))
+        .port(config.get('port'), 3306)
+        .volume(config.get('folder'), '/var/lib/mysql')
+        .volume(config.getUserDir('conf.d'), '/etc/mysql/conf.d')
+        .volume(config.getUserDir('home'), '/root')
+        .env('MYSQL_ROOT_PASSWORD', config.get('rootPassword'))
+        .env('TZ', configGlobal.get('timezone'))
+        .isDetached(true)
+        .isRemove(true)
+        .imageName(config.get('image'))
+    ;
+}
+
+module.exports = {
+    setup: function()
+    {
+        config.setEmpty('image', 'mysql:5.7');
+        config.setEmpty('folder', config.getDataDir());
+        config.setEmpty('port', 3306);
+        config.setEmpty('rootPassword', 'password');
+
+        config.copyToUserDir(__dirname + '/mysql/conf.d');
+        config.copyToUserDir(__dirname + '/mysql/home');
+    },
+
+    debugcli() {
+        console.log('docker ' + getContainerDef().buildConsole().join(' '));
+    },
+
+    debugapi() {
+        console.log(getContainerDef().buildApi());
+    },
+
+    up: function()
+    {
+        usdockerhelper.up(getContainerDef());
+    },
+
+    status: function() {
+        usdockerhelper.status(SCRIPTNAME);
+    },
+
+    down: function()
+    {
+        usdockerhelper.down(SCRIPTNAME);
+    },
+
+    help: function () {
+
+        showdocs.getDocumentation(__dirname);
+    },
+};
